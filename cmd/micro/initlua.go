432a8d25e80d4e191bf8c4c95651a93ef814a6e4
matthias314 <matthias314@posteo.net>
2024-11-28 20:26:17 -0500

    added `NewLogBuffer` and `NewScratchBuffer`

--- cmd/micro/initlua.go
+++ cmd/micro/initlua.go
@@ -135,6 +135,12 @@ func luaImportMicroBuffer() *lua.LTable {
 	ulua.L.SetField(pkg, "NewBuffer", luar.New(ulua.L, func(text, path string) *buffer.Buffer {
 		return buffer.NewBufferFromString(text, path, buffer.BTDefault)
 	}))
+	ulua.L.SetField(pkg, "NewLogBuffer", luar.New(ulua.L, func(text, path string) *buffer.Buffer {
+		return buffer.NewBufferFromString(text, path, buffer.BTLog)
+	}))
+	ulua.L.SetField(pkg, "NewScratchBuffer", luar.New(ulua.L, func(text, path string) *buffer.Buffer {
+		return buffer.NewBufferFromString(text, path, buffer.BTScratch)
+	}))
 	ulua.L.SetField(pkg, "NewBufferFromFile", luar.New(ulua.L, func(path string) (*buffer.Buffer, error) {
 		return buffer.NewBufferFromFile(path, buffer.BTDefault)
 	}))
