76b8d6cd06e9c5e0385bed7ca04d44afeebe6ca2
matthias314 <matthias314@posteo.net>
2024-12-05 16:18:23 -0500

    modified `Autocomplete`

--- internal/action/actions.go
+++ internal/action/actions.go
@@ -925,16 +925,17 @@ func (h *BufPane) Autocomplete() bool {
 	if h.Cursor.X == 0 {
 		return false
 	}
+
+	if b.HasSuggestions {
+		b.CycleAutocomplete(true)
+		return true
+	}
+
 	r := h.Cursor.RuneUnder(h.Cursor.X)
 	prev := h.Cursor.RuneUnder(h.Cursor.X - 1)
 	if !util.IsAutocomplete(prev) || util.IsWordChar(r) {
 		// don't autocomplete if cursor is within a word
 		return false
-	}
-
-	if b.HasSuggestions {
-		b.CycleAutocomplete(true)
-		return true
 	}
 	return b.Autocomplete(buffer.BufferComplete)
 }
