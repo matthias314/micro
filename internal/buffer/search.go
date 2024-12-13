3e16d5b4c14c8a4adc4ae40b3ac63df2bda6b541
matthias314 <matthias314@posteo.net>
2024-12-13 11:26:29 -0500

    match beginning and end of line correctly

--- internal/buffer/search.go
+++ internal/buffer/search.go
@@ -6,6 +6,50 @@ import (
 	"github.com/zyedidia/micro/v2/internal/util"
 )
 
+const (
+	padStart = 1 << iota
+	padEnd
+)
+
+// We want "^" and "$" to match only the beginning/end of a line, not the
+// beginning/end of the search region if it is in the middle of a line.
+// In that case we use padded regexps to require a rune before or after
+// the match. (This also affects other empty-string patters like "\\b".)
+// The function padRegexp creates these padded regexps.
+func padRegexp(r *regexp.Regexp) [4]*regexp.Regexp {
+	rPadStart := regexp.MustCompile(".(?:" + r.String() + ")")
+	rPadEnd := regexp.MustCompile("(?:" + r.String() + ").")
+	rPadBoth := regexp.MustCompile(".(?:" + r.String() + ").")
+	return [4]*regexp.Regexp{r, rPadStart, rPadEnd, rPadBoth}
+}
+
+func findLineParams(b *Buffer, start, end Loc, i int) ([]byte, int, int) {
+	l := b.LineBytes(i)
+	charpos := 0
+	padMode := 0
+
+	if i == end.Y {
+		nchars := util.CharacterCount(l)
+		end.X = util.Clamp(end.X, 0, nchars)
+		if end.X < nchars {
+			l = util.SliceStart(l, end.X+1)
+			padMode |= padEnd
+		}
+	}
+
+	if i == start.Y {
+		nchars := util.CharacterCount(l)
+		start.X = util.Clamp(start.X, 0, nchars)
+		if start.X > 0 {
+			charpos = start.X - 1
+			l = util.SliceEnd(l, charpos)
+			padMode |= padStart
+		}
+	}
+
+	return l, charpos, padMode
+}
+
 func (b *Buffer) findDown(r *regexp.Regexp, start, end Loc) ([2]Loc, bool) {
 	lastcn := util.CharacterCount(b.LineBytes(b.LinesNum() - 1))
 	if start.Y > b.LinesNum()-1 {
@@ -21,33 +65,22 @@ func (b *Buffer) findDown(r *regexp.Regexp, start, end Loc) ([2]Loc, bool) {
 		start, end = end, start
 	}
 
+	rPadded := padRegexp(r)
+
 	for i := start.Y; i <= end.Y; i++ {
-		l := b.LineBytes(i)
-		charpos := 0
+		l, charpos, padMode := findLineParams(b, start, end, i)
 
-		if i == start.Y && start.Y == end.Y {
-			nchars := util.CharacterCount(l)
-			start.X = util.Clamp(start.X, 0, nchars)
-			end.X = util.Clamp(end.X, 0, nchars)
-			l = util.SliceStart(l, end.X)
-			l = util.SliceEnd(l, start.X)
-			charpos = start.X
-		} else if i == start.Y {
-			nchars := util.CharacterCount(l)
-			start.X = util.Clamp(start.X, 0, nchars)
-			l = util.SliceEnd(l, start.X)
-			charpos = start.X
-		} else if i == end.Y {
-			nchars := util.CharacterCount(l)
-			end.X = util.Clamp(end.X, 0, nchars)
-			l = util.SliceStart(l, end.X)
-		}
-
-		match := r.FindIndex(l)
+		match := rPadded[padMode].FindIndex(l)
 
 		if match != nil {
 			start := Loc{charpos + util.RunePos(l, match[0]), i}
+			if padMode&padStart != 0 {
+				start = start.Move(1, b)
+			}
 			end := Loc{charpos + util.RunePos(l, match[1]), i}
+			if padMode&padEnd != 0 {
+				end = end.Move(-1, b)
+			}
 			return [2]Loc{start, end}, true
 		}
 	}
@@ -69,34 +102,23 @@ func (b *Buffer) findUp(r *regexp.Regexp, start, end Loc) ([2]Loc, bool) {
 		start, end = end, start
 	}
 
+	rPadded := padRegexp(r)
+
 	for i := end.Y; i >= start.Y; i-- {
-		l := b.LineBytes(i)
-		charpos := 0
+		l, charpos, padMode := findLineParams(b, start, end, i)
 
-		if i == start.Y && start.Y == end.Y {
-			nchars := util.CharacterCount(l)
-			start.X = util.Clamp(start.X, 0, nchars)
-			end.X = util.Clamp(end.X, 0, nchars)
-			l = util.SliceStart(l, end.X)
-			l = util.SliceEnd(l, start.X)
-			charpos = start.X
-		} else if i == start.Y {
-			nchars := util.CharacterCount(l)
-			start.X = util.Clamp(start.X, 0, nchars)
-			l = util.SliceEnd(l, start.X)
-			charpos = start.X
-		} else if i == end.Y {
-			nchars := util.CharacterCount(l)
-			end.X = util.Clamp(end.X, 0, nchars)
-			l = util.SliceStart(l, end.X)
-		}
-
-		allMatches := r.FindAllIndex(l, -1)
+		allMatches := rPadded[padMode].FindAllIndex(l, -1)
 
 		if allMatches != nil {
 			match := allMatches[len(allMatches)-1]
 			start := Loc{charpos + util.RunePos(l, match[0]), i}
+			if padMode&padStart != 0 {
+				start = start.Move(1, b)
+			}
 			end := Loc{charpos + util.RunePos(l, match[1]), i}
+			if padMode&padEnd != 0 {
+				end = end.Move(-1, b)
+			}
 			return [2]Loc{start, end}, true
 		}
 	}
